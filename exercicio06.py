8e0603ced54f7c12ceacbd60504398c846beb30c
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-04-10 10:55:18 -0300

    otimização do código

--- exercicio06.py
+++ exercicio06.py
@@ -4,6 +4,5 @@ for i in range(10):
     n = int(input("Digite um número: "))
     if n >= 10 and n <= 20:
         dentroN += 1
-    else:
-        foraN += 1
-print(f"No intervalo entre 10 e 20 tem {dentroN} números, e fora tem {foraN} números.")
+foraN = 10-dentroN
+print(f"No interv0alo entre 10 e 20 tem {dentroN} números, e fora tem {foraN} números.")
